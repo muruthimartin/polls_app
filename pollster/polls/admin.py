8ee0a97873c54e0c86c06ec9000bb4dd711bd499
Martin Muruthi <muruthimartin@gmail.com>
2020-09-16 18:54:58 +0300

    update admin site title and headers

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.site_header="Pollster Admin"
-admin.site.site_title="Pollster Admin Area"
-admin.site.index_title="Welcome to Pollster Admin Area"
+admin.site.site_header="Polls App Admin"
+admin.site.site_title="Polls App Admin Area"
+admin.site.index_title="Welcome to The Polls App Admin Area"
 
 
 
